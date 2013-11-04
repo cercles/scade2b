4c0cb625f8df467127073abb78485753edb550df
FlorianThibord <florian.thibord@gmail.com>
2013-11-04 13:50:42 +0100

    update 411 morning

--- scade2b/scade2b.ml
+++ scade2b/scade2b.ml
@@ -80,10 +80,13 @@ let () =
   in
   close_in channel;  
 
+  (* Création du répertoire contenant les fichiers de sorties *)
+  if not(Sys.file_exists main_dir) then Unix.mkdir main_dir 0o764; 
+
   let id_consts = List.map (fun cst -> cst.c_id) prog.const_list in
 
   (* Traduction de chaque noeud du programme *)
-  let node_translator node_name node =
+  let node_translator node_name node xml_map =
     let import_list = 
       try 
 	XML_prog.find node_name xml_map 
@@ -94,15 +97,16 @@ let () =
     try
       let ast = Parser.prog Lexer.token lexbuf in
       let ast_n = Normalizer.normalize_node ast prog.const_list in
-      let ast_b = Trad.translate ast_n (List.map (fun {node_name=ident; params_m=param} -> ident) import_list) id_consts in   
+      let ast_b = Trad.translate ast_n (List.map (fun {node_name=ident; params_m=param} -> ident) import_list) id_consts in
       let babst_file = 
-	open_out (Filename.concat (Filename.dirname main_dir) ("M_" ^ node_name ^ ".mch")) in
+	open_out (Filename.concat main_dir ("M_" ^ node_name ^ ".mch")) in
       Babst_generator.print_prog ast_b.machine_abstraite babst_file;
       let bimpl_file = 
-	open_out (Filename.concat (Filename.dirname main_dir) ("M_" ^ node_name ^ "_i.imp")) in
+	open_out (Filename.concat main_dir ("M_" ^ node_name ^ "_i.imp")) in
       Bimpl_generator.print_prog ast_b.implementation bimpl_file;
       close_out babst_file;
-      close_out bimpl_file
+      close_out bimpl_file;
+      xml_map
     with
       | Lexer.Lexical_error s ->
 	  Format.eprintf "Lexical Error: %s\n@." s;
@@ -113,20 +117,19 @@ let () =
 	  handle_error (lexeme_start_p lexbuf, lexeme_end_p lexbuf);
 	  exit 1
       | Normalizer.Assert_id_error e ->
-	  Format.eprintf "Error: Assert  %s.\n@." e
+	  Format.eprintf "Error: Assert  %s.\n@." e;
+	  exit 1
       | Normalizer.Ident_Call_Error e ->
-	  Format.eprintf "Error: The node name %s is reserved in B.\n@." e
+	  Format.eprintf "Error: The node name %s is reserved in B.\n@." e;
+	  exit 1
       | Trad.Register_cond_error e ->
-	  Format.eprintf "Register condition error: %s isn't related to an input/output \n@." e
+	  Format.eprintf "Register condition error: %s isn't related to an input/output \n@." e;
+	  exit 1
       | e ->
 	  Format.eprintf "Anomaly: %s\n@." (Printexc.to_string e);
 	  exit 2
   in
 
-
-  (* Création du répertoire contenant les fichiers de sorties *)
-
-  if not(Sys.file_exists main_dir) then Unix.mkdir main_dir 0o764; 
 
   (* Création d'une machine pour les constantes. *)
   let bconst_file = 
@@ -139,18 +142,28 @@ let () =
   let node_list =
     let xml_imports_list = XML_prog.bindings xml_map in
     let remove_param_from_imports l =
-      List.map (fun (elt, l2) -> List.map (fun {node_name=ident; params_m=param} -> ident) l2) l 
+      List.map (fun (elt, l2) -> elt, (List.map (fun {node_name=ident; params_m=param} -> ident)) l2) l 
     in
     let to_schedule = remove_param_from_imports xml_imports_list in
     let rec scheduler not_ordered ordered =
       match not_ordered with
       | [] -> ordered
-      | (node, imports) :: l ->  if List.for_all (fun ident -> List.mem ident ordered) imports 
-	then scheduler not_ordered (ordered @ [node])
+      | (node, imports) :: l -> if List.for_all (fun ident -> List.mem ident ordered) imports
+	then scheduler l (ordered @ [node])
 	else scheduler (l @ [(node, imports)]) ordered
     in
     scheduler to_schedule []
   in
-      
-      
-  T_Node.iter (fun name node -> if XML_prog.mem name xml_map then node_translator name node) prog.node_map
+        
+  (* List.iter (fun node_ident ->  *)
+  (* 	       try node_translator node_ident (T_Node.find node_ident prog.node_map xml_map) *)
+  (* 	       with Not_found -> Printf.printf "Scade2b error, node_ident not found in node_map\n" *)
+  (* 	    ) node_list *)
+
+  ignore(
+  List.fold_left (fun map node_ident -> 
+		    try node_translator node_ident (T_Node.find node_ident prog.node_map) map
+		    with Not_found -> (Printf.printf "Scade2b error, node_ident not found in node_map\n"; map)
+		 ) xml_map node_list)
+
+(* AJOUTER une fonction dans utils de mise à jour de la map. *)
