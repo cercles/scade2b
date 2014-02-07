85a387b96def9aafe63a97885410fda328e59b43
Etienne Millon <me@emillon.org>
2014-02-07 10:12:16 +0100

    dedup print_expr

--- scade2b/printer.ml
+++ scade2b/printer.ml
@@ -1,4 +1,5 @@
 open Ast_base
+open Ast_repr_b
 open Ast_scade_norm
 open Format
 
@@ -53,3 +54,64 @@ let print_op_logic ppt = function
   | Op_and -> fprintf ppt "&"
   | Op_or -> fprintf ppt "or"
   | Op_xor -> assert false
+
+let rec print_e_list ppt = function
+  | [] -> ()
+  | [v] -> fprintf ppt "%a" print_expr v
+  | v::l -> fprintf ppt "%a, %a" print_expr v print_e_list l
+
+and print_expr ppt = function
+  | BE_Ident id -> print_bid ppt id
+  | BE_Value v -> print_value ppt v
+  | BE_Array ar -> print_array ppt ar
+  | BE_Op_Arith1 (op, e) -> (
+      match op with
+	| Op_minus ->
+	    fprintf ppt "(%a%a)" print_op_arith1 op print_expr e
+	| Op_cast_real | Op_cast_int ->
+	    fprintf ppt "(%a (%a))" print_op_arith1 op print_expr e
+    )
+  | BE_Op_Arith2 (op, e1, e2) -> (
+      match op with
+	| Op_eq | Op_neq | Op_lt | Op_le | Op_gt | Op_ge ->
+            fprintf ppt "bool(%a %a %a)" print_expr e1 print_op_arith2 op print_expr e2
+        | _ -> fprintf ppt "%a %a %a" print_expr e1 print_op_arith2 op print_expr e2
+    )
+  | BE_Op_Sharp e_list ->
+      fprintf ppt "sharp(%a)" print_e_list e_list
+  | BE_Op_Logic (Op_xor, e1, e2) ->
+      fprintf ppt "%a /= %a" print_expr e1 print_expr e2
+  | BE_Op_Not e ->
+      fprintf ppt "bool(not (%a = TRUE))" print_expr e
+  | BE_Op_Logic (op, e1, e2) ->
+      fprintf ppt "bool(%a = TRUE %a %a = TRUE)" print_expr e1 print_op_logic op print_expr e2
+
+and print_array ppt = function
+  | BA_Def e_list -> fprintf ppt "{%a}" print_def_list e_list
+  | BA_Index (id, e_list) -> fprintf ppt "%a(%a)" print_bid id print_expr_list e_list
+  | BA_Caret (e1, e2) -> fprintf ppt "caret(%a, %a)" print_expr e1 print_expr e2
+  | BA_Concat (e1, e2) -> fprintf ppt "concat(%a, %a)" print_expr e1 print_expr e2
+  | BA_Slice (id, e_list) -> fprintf ppt "slice(%a, %a)" print_bid id print_slice_list e_list
+
+and print_def_list ppt e_list =
+  let rec fun_rec n ppt = function
+    | [] -> ()
+    | [v] -> fprintf ppt "%d |-> %a" n print_expr v
+    | v::l -> fprintf ppt "%d |-> %a, %a" n print_expr v (fun_rec (n+1)) l
+  in
+  fun_rec 0 ppt e_list
+
+and print_slice_list ppt = function
+  | [] -> ()
+  | [(e1, e2)] -> fprintf ppt "(%a, %a)" print_expr e1 print_expr e2
+  | (e1, e2)::l -> fprintf ppt "(%a, %a), %a" print_expr e1 print_expr e2 print_slice_list l
+
+and print_expr_list ppt = function
+  | [] -> ()
+  | [(e)] -> fprintf ppt "%a" print_expr e
+  | (e)::l -> fprintf ppt "%a, %a" print_expr e print_expr_list l
+
+let rec print_index_list ppt = function
+  | [] -> ()
+  | [(e)] -> fprintf ppt "%a" print_expr e
+  | (e)::l -> fprintf ppt "%a, %a" print_expr e print_index_list l
